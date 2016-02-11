c4fe1247280c96b6cb9fa94951d017a1a06b324f
Dennise Saxton <dennisepnd@gmail.com>
2016-02-11 16:47:34 -0600

    Added yo heroku

--- Gruntfile.js
+++ Gruntfile.js
@@ -226,6 +226,24 @@ module.exports = function (grunt) {
       }
     },
 
+    copy: {
+        dist: {
+            files: [{
+                expand: true,
+                dest: '<%= yeoman.dist %>',
+                cwd: 'heroku',
+                src: '*',
+                rename: function (dest, src) {
+                    var path = require('path');
+                    if (src === 'distpackage.json') {
+                        return path.join(dest, 'package.json');
+                    }
+                    return path.join(dest, src);
+                }
+            }]
+        }
+    },
+
     // Compiles Sass to CSS and generates necessary files if requested
     compass: {
       options: {
